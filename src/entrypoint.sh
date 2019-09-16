7f73e21c93fa2505b1949581d1b8625adc4cb4b1
Rob Vadai <rvadai@segence.com>
2019-09-16 17:20:26 +0800

    Execution options (#2)
    * Environment variables can be used to configure runtime options
    
    * Updated README with execution options
    
    * Supporting configuration via environment variables and by mounting the configuration file
    
    * Helm chart supports additional exec options

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,12 +1,51 @@
 #!/usr/bin/env sh
 
+DRILL_OVERRIDE_CONF=${DRILL_HOME}/conf/drill-override.conf
+DRILL_EXEC_OPTIONS_PREFIX=DRILL_EXEC_
+
+function set_zookeeper_host {
+    sed -i "s/zk.*/zk.connect: \"${1}\"/g" ${DRILL_OVERRIDE_CONF}
+}
+
+function set_exec_options {
+
+    DRILL_EXEC_OPTIONS=$(printenv | grep "^${DRILL_EXEC_OPTIONS_PREFIX}.*$" | wc -l)
+
+    if [[ "${DRILL_EXEC_OPTIONS}" > "0" ]]; then
+
+        echo 'drill.exec.options: {' >> ${DRILL_OVERRIDE_CONF}
+
+        DRILL_CONFIGS=$(printenv | grep "^${DRILL_EXEC_OPTIONS_PREFIX}.*$" | awk -F= '{print $1}')
+        for config_name in ${DRILL_CONFIGS}; do
+            config_key=$(echo ${config_name:${#DRILL_EXEC_OPTIONS_PREFIX}} | sed 's/_/./g' | awk '{print tolower($0)}')
+            config_value=$(printenv ${config_name})
+            echo "    ${config_key}: ${config_value}" >> ${DRILL_OVERRIDE_CONF}
+        done
+
+        echo '}' >> ${DRILL_OVERRIDE_CONF}
+    fi
+}
+
+function start_embedded {
+    echo 'Starting in embedded mode...'
+    ${DRILL_HOME}/bin/drill-embedded
+}
+
+function start_cluster {
+    echo 'Starting in clustered mode...'
+    ${DRILL_HOME}/bin/drillbit.sh run
+}
+
 sed -i "s/export DRILLBIT_OPTS=\"-Xms.*/export DRILLBIT_OPTS=\"-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=${HEAP_MEMORY_FRACTION:-2}\"/g" ${DRILL_HOME}/bin/drill-config.sh
 
-if [[ -z "${ZOOKEEPER_HOST}" ]]; then
-    echo 'No Zookeeper configured, starting in embedded mode...'
-    ${DRILL_HOME}/bin/drill-embedded
+set_exec_options
+
+if [[ -n "${CLUSTERED_MODE}" && "${CLUSTERED_MODE}" == "true" ]]; then
+    if [[ -n "${ZOOKEEPER_HOST}" ]]; then
+        echo "Zookeeper host configured at ${ZOOKEEPER_HOST}, updating configuration..."
+        set_zookeeper_host ${ZOOKEEPER_HOST}
+    fi
+    start_cluster
 else
-    echo "Zookeeper host configured at ${ZOOKEEPER_HOST}, starting in clustered mode..."
-    sed -i "s/zk.*/zk.connect: \"${ZOOKEEPER_HOST}\"/g" ${DRILL_HOME}/conf/drill-override.conf
-    ${DRILL_HOME}/bin/drillbit.sh run
+    start_embedded
 fi
